haubi/cygwin-rebase

commit a7d415a25c1b902a98dbc2b0e2fd8928b58e3f61

handle "out of available DLL slots" condition
Signed-off-by: Corinna Vinschen <vinschen@redhat.com>

rebase.c
@@ -243,7 +243,8 @@ main (int argc, char *argv[])
       /* Rebase with database support. */
       BOOL header;
 
-      merge_image_info ();
+      if (merge_image_info () < 0)
+	return 2;
       status = TRUE;
       for (i = 0; i < img_info_size; ++i)
 	if (img_info_list[i].flag.needs_rebasing)
@@ -745,11 +746,20 @@ merge_image_info ()
       ULONG64 new_base;
 
       /* Skip trailing entries as long as there is no hole. */
-       while (img_info_list[end].base + img_info_list[end].slot_size + offset
-	     >= floating_image_base)
+       while (end > 0
+	      && img_info_list[end].base + img_info_list[end].slot_size
+		 + offset >= floating_image_base)
 	{
 	  floating_image_base = img_info_list[end].base;
 	  --end;
+	}
+      /* No hole?  We're in serious trouble! */
+      if (end <= 0)
+	{
+	  fprintf (stderr,
+		   "%s: Too many DLLs for available address space: %s\n",
+		   progname, strerror (ENOMEM));
+	  return -1;
 	}
       /* Test if one of the DLLs with address 0 fits into the hole. */
       for (i = 0, new_base = 0; img_info_list[i].base == 0; ++i, new_base = 0)

commit f5eb52bf39fb64e45282adf6afbe78371989631a

Patch "out of available DLL slots" patch
Commit a7d415a25c1b902a98dbc2b0e2fd8928b58e3f61 introduced
a typo which handled the completly innocent situation of
reaching the first array member (end ==0) as error.  Fix this
and only handle indices < 0 as out of slot error.

Also, on 64 bit, don't allow rebasing DLLs beneath the Cygwin
DLL address since that's reserved for stacks anyway.

Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

rebase.c
@@ -754,7 +754,7 @@ merge_image_info ()
 	  --end;
 	}
       /* No hole?  We're in serious trouble! */
-      if (end <= 0)
+      if (end < 0)
 	{
 	  fprintf (stderr,
 		   "%s: Too many DLLs for available address space: %s\n",
@@ -793,7 +793,17 @@ merge_image_info ()
 #if defined(__CYGWIN__) || defined(__MSYS__)
       if (floating_image_base >= cygwin_dll_image_base + cygwin_dll_image_size
 	  && img_info_list[end].base < cygwin_dll_image_base)
-	floating_image_base = cygwin_dll_image_base;
+	{
+	  if (machine == IMAGE_FILE_MACHINE_I386)
+	    floating_image_base = cygwin_dll_image_base;
+	  else /* No DLLs below Cygwin, ever, on 64 bit. */
+	    {
+	      fprintf (stderr,
+		       "%s: Too many DLLs for available address space: %s\n",
+		       progname, strerror (ENOMEM));
+	      return -1;
+	    }
+	}
       else
 #endif
 	{